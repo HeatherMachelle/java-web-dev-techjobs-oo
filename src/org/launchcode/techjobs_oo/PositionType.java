d7336c5454b590969f5b2b8c1c9eb96160bf3218
heathersmith <heathermachelle.s@gmail.com>
2021-02-10 17:29:50 -0600

    1-3 some errors but on to test

--- src/org/launchcode/techjobs_oo/PositionType.java
+++ src/org/launchcode/techjobs_oo/PositionType.java
@@ -1,5 +1,7 @@
 package org.launchcode.techjobs_oo;
 
+import java.util.Objects;
+
 public class PositionType {
     private int id;
     private static int nextId = 1;
@@ -17,8 +19,16 @@ public class PositionType {
 
     // TODO: Add a custom toString() method that returns the data stored in 'value'.
 
+    public PositionType(String value)
+    { super(value); }
+
     // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
     //  their id fields match.
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
 
     // Getters and Setters:
 
